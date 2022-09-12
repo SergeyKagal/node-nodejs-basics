5e8032a60e8187de4b047818cb62bf638bf21efb
SergeyKagal <3461995@gmail.com>
2022-09-12 23:15:17 +0300

    feat: env cli

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,10 @@
 export const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const myEnv = process.env;
+
+  for (const key in myEnv) {
+    console.log(`RSS_${key}=${myEnv[key]}`);
+  }
 };
+
+parseEnv();
