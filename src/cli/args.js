ee1bfe95e8a6ad9d653544fe878b70c681b21521
SergeyKagal <3461995@gmail.com>
2022-09-12 23:15:18 +0300

    feat: args cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,10 @@
 export const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const cliArgs = process.argv;
+  cliArgs.forEach((el, i) => {
+    if (el.includes('--')) {
+      console.log(`${el} is ${cliArgs[i + 1]}`);
+    }
+  });
 };
+parseArgs();
