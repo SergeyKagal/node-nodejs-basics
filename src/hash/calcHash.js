cccfd3e75ca6d5c2436ab764419edff987f0247d
SergeyKagal <3461995@gmail.com>
2022-09-12 23:15:19 +0300

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,16 @@
+import { readFile } from 'fs/promises';
+import { resolve } from 'path';
+import { createHash } from 'crypto';
+
+const file = 'fileToCalculateHashFor.txt';
+const srcFolder = 'files';
+const src = resolve(srcFolder, file);
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const data = await readFile(src, 'utf-8');
+  const hash = createHash('sha256').update(data).digest('hex');
+
+  return hash;
 };
+
+calculateHash().then((data) => console.log(data));
