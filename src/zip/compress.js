0390df90136200090188944c525a8c142f5ef4a5
SergeyKagal <3461995@gmail.com>
2022-09-12 23:15:20 +0300

    feat: zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,26 @@
+import { resolve } from 'path';
+import { createGzip as gzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+// eslint-disable-next-line node/no-unsupported-features/node-builtins
+import { pipeline } from 'stream';
+
+const fileSrc = 'fileToCompress.txt';
+const fileDist = 'archive.gz';
+const folder = 'files';
+const src = resolve(folder, fileSrc);
+const dist = resolve(folder, fileDist);
+
 export const compress = async () => {
-    // Write your code here 
+  const readStream = createReadStream(src);
+  const writeStream = createWriteStream(dist);
+
+  pipeline(readStream, gzip(), writeStream, (err) => {
+    if (err) {
+      console.log(err.message);
+      throw err;
+    } else {
+      console.log('Succes');
+    }
+  });
 };
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,24 @@
+import { resolve } from 'path';
+import { createGunzip as unzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+// eslint-disable-next-line node/no-unsupported-features/node-builtins
+import { pipeline } from 'stream';
+
+const fileSrc = 'archive.gz';
+const fileDist = 'unZiped.txt';
+const folder = 'files';
+const src = resolve(folder, fileSrc);
+const dist = resolve(folder, fileDist);
+
 export const decompress = async () => {
-    // Write your code here 
+  const readStream = createReadStream(src);
+  const writeStream = createWriteStream(dist);
+  pipeline(readStream, unzip(), writeStream, (err) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log('succes');
+    }
+  });
 };
+decompress();
