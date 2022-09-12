abe779ba9a8ef7342b991f952dabe6fb0ee9b648
SergeyKagal <3461995@gmail.com>
2022-09-12 23:15:16 +0300

    feat: delete fs

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,16 @@
+import * as fs from 'fs/promises';
+import { resolve } from 'path';
+
+const fileName = 'fileToRemove.txt';
+const dist = 'files';
+
 export const remove = async () => {
-    // Write your code here 
+  return fs
+    .rm(resolve(dist, fileName))
+    .then(() => console.log(`${fileName} is deleted succesfuly`))
+    .catch(() => {
+      throw new Error('FS operation failed');
+    });
 };
+
+remove();
