4700dd420cd996ce70fc9e50eca35b0082fa80e8
SergeyKagal <3461995@gmail.com>
2022-09-12 23:15:18 +0300

    fix: create fs

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,57 +1,19 @@
-import { writeFile, open, close } from 'fs';
+// import { writeFile, open, close } from 'fs';
+import { writeFile as asyncWriteFile } from 'fs/promises';
 import * as path from 'path';
 
 const fileName = 'fresh.txt';
 const content = 'I am fresh and young';
 const dist = path.resolve('files', fileName);
+const error = new Error('FS operation failed');
 
 console.log(dist, content);
 
-export const create = async () => {
-  // Write your code here
-  return new Promise((res, rej) => {
-    open(dist, 'wx', (err, fd) => {
-      if (err) {
-        rej(new Error('FS operation failed'));
-      } else {
-        console.log('file opened');
-        writeFile(fd, content, (err) => {
-          if (err) {
-            throw new Error('FS operation failed');
-          } else console.log(' file is written ');
-        });
-        close(fd, (err) => {
-          if (err) {
-            throw new Error('FS operation failed');
-          } else console.log('file closed');
-        });
-        res(
-          (() => {
-            console.log('Done !!!');
-          })()
-        );
-      }
+const create = () => {
+  asyncWriteFile(dist, content, { flag: 'wx' })
+    .then(() => console.log(`${fileName} is created`))
+    .catch(() => {
+      throw error;
     });
-  });
 };
 create();
-export const foo = () => {
-  open(dist, 'wx', (err, fd) => {
-    if (err) {
-      throw new Error('FS operation failed');
-    } else {
-      console.log('file opened');
-      writeFile(fd, content, (err) => {
-        if (err) {
-          throw new Error('FS operation failed');
-        } else console.log(' file is written ');
-      });
-      close(fd, (err) => {
-        if (err) {
-          throw new Error('FS operation failed');
-        } else console.log('file closed');
-      });
-    }
-  });
-};
-// foo();
