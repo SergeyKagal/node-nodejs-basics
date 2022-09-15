0dd907224c9f379a62821e7fb0a127a2918e4363
SergeyKagal <3461995@gmail.com>
2022-09-16 01:57:23 +0300

    feat: wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,32 @@
+/* eslint-disable no-unused-vars */
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+import { resolve } from 'path';
+
+const workerUrl = resolve('worker.js');
+
 export const performCalculations = async () => {
-    // Write your code here
+  // Write your code here
+  const cpusArr = cpus();
+  let num = 10;
+
+  const workers = cpusArr.map(() => {
+    return new Promise((res, rej) => {
+      const worker = new Worker(workerUrl, {
+        workerData: num++,
+      });
+      worker.on('message', (msg) => res(msg));
+      worker.on('error', (err) => rej(err));
+    });
+  });
+
+  return Promise.allSettled(workers)
+    .then((resArr) => {
+      return resArr.map(({ status, value }) => ({
+        status: status === 'fulfilled' ? 'resolved' : 'error',
+        data: status === 'fulfilled' ? value : null,
+      }));
+    })
+    .then((result) => console.log(result));
 };
+performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,13 @@
+// eslint-disable-next-line node/no-unsupported-features/node-builtins
+import { workerData, parentPort } from 'worker_threads';
 // n should be received from main thread
-export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+export const nthFibonacci = (n) => {
+  return n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+};
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  if (Math.random() > 0.5) throw new Error('oops');
+  // This function sends result of nthFibonacci computations to main thread
+  parentPort.postMessage(nthFibonacci(workerData));
 };
+sendResult();
